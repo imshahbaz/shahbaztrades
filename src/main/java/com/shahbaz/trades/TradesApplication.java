1ee4a5a6b1dcc236b122c36b63b57be41005647a
imshahbaz21 <imshahbaz21@gmail.com>
2025-12-16 00:43:00 +0530

    changing java version

--- src/main/java/com/shahbaz/trades/TradesApplication.java
+++ src/main/java/com/shahbaz/trades/TradesApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class TradesApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(TradesApplication.class, args);
     }
 
