3aa7b962bd63813340724643de822940902c5ebd
imshahbaz21 <imshahbaz21@gmail.com>
2025-12-22 14:30:37 +0530

    fix in margin

--- src/main/java/com/shahbaz/trades/repository/MarginRepository.java
+++ src/main/java/com/shahbaz/trades/repository/MarginRepository.java
@@ -2,8 +2,14 @@ package com.shahbaz.trades.repository;
 
 import com.shahbaz.trades.model.entity.Margin;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MarginRepository extends MongoRepository<Margin, String> {
+
+    @Query(value = "{ '_id' : { '$nin' : ?0 } }", delete = true)
+    long deleteByIdNotIn(List<String> ids);
 }
