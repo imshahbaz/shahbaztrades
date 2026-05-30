0ff3eed57e36304c75fed0d6c5ba1cd2511c6099
imshahbaz21 <imshahbaz21@gmail.com>
2026-05-30 18:34:13 +0530

    redis fix

--- src/main/java/com/app/shahbaztrades/config/redis/RedisConfig.java
+++ src/main/java/com/app/shahbaztrades/config/redis/RedisConfig.java
@@ -1,6 +1,8 @@
 package com.app.shahbaztrades.config.redis;
 
 import com.app.shahbaztrades.service.MongoConfigService;
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +10,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+import java.time.Duration;
 
 @Configuration
 @RequiredArgsConstructor
@@ -26,7 +30,19 @@ public class RedisConfig {
         config.setUsername(user);
         config.setPassword(pass);
 
+        SocketOptions socketOptions = SocketOptions.builder()
+                .connectTimeout(Duration.ofSeconds(10))
+                .keepAlive(true)
+                .build();
+
+        ClientOptions clientOptions = ClientOptions.builder()
+                .socketOptions(socketOptions)
+                .disconnectedBehavior(ClientOptions.DisconnectedBehavior.DEFAULT)
+                .build();
+
         LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .clientOptions(clientOptions)
+                .commandTimeout(Duration.ofSeconds(5))
                 .useSsl()
                 .build();
 
