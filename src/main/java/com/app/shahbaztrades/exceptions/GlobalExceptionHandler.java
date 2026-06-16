8e38d380e849e4e2cf36ff8f92d4ff19537041c4
imshahbaz21 <imshahbaz21@gmail.com>
2026-06-17 01:44:33 +0530

    added handler

--- src/main/java/com/app/shahbaztrades/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/app/shahbaztrades/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.app.shahbaztrades.exceptions;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @RestControllerAdvice
@@ -50,6 +52,13 @@ public class GlobalExceptionHandler {
     public ProblemDetail handleForbidden(ForbiddenException ex) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, ex.getMessage());
         problemDetail.setTitle("Forbidden");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ProblemDetail handleNoResourceFound(NoResourceFoundException ex, HttpServletRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, "The requested resource was not found.");
+        problemDetail.setTitle("Not Found");
         return problemDetail;
     }
 
