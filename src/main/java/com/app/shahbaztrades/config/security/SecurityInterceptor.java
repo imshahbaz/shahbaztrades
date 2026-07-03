c9344bf8a41289ddcd9cf1f51083ad208ef9ac59
imshahbaz21 <imshahbaz21@gmail.com>
2026-07-03 09:10:06 +0530

    jwt token rolling expiry

--- src/main/java/com/app/shahbaztrades/config/security/SecurityInterceptor.java
+++ src/main/java/com/app/shahbaztrades/config/security/SecurityInterceptor.java
@@ -4,21 +4,32 @@ import com.app.shahbaztrades.exceptions.ForbiddenException;
 import com.app.shahbaztrades.exceptions.UnauthorizedException;
 import com.app.shahbaztrades.model.dto.UserDto;
 import com.app.shahbaztrades.model.enums.UserRole;
+import com.app.shahbaztrades.util.HelperUtil;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.jspecify.annotations.NonNull;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.time.Duration;
+import java.util.Objects;
+
+import static com.app.shahbaztrades.util.Constants.ENV_PRODUCTION;
 
 @Component
 @RequiredArgsConstructor
 public class SecurityInterceptor implements HandlerInterceptor {
 
+    private static final long ROLLING_EXPIRY_THRESHOLD = Duration.ofHours(12).toMillis();
+
     private final JwtService jwtService;
+    private final Environment environment;
 
     @Override
     public boolean preHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) {
@@ -41,6 +52,7 @@ public class SecurityInterceptor implements HandlerInterceptor {
                 var claims = jwtService.validateToken(c.getValue());
                 userDto = claims.getUser();
                 request.setAttribute("user", claims.getUser());
+                rollToken(response, userDto, claims.getExpiresAt());
                 break;
             }
         }
@@ -55,4 +67,14 @@ public class SecurityInterceptor implements HandlerInterceptor {
 
         return true;
     }
+
+    private void rollToken(@NonNull HttpServletResponse response, UserDto userDto, long previousExpiry) {
+        var timeLeft = previousExpiry - System.currentTimeMillis();
+        if (timeLeft < ROLLING_EXPIRY_THRESHOLD) {
+            var tokenStr = jwtService.generateToken(userDto);
+            var cookie = HelperUtil.createAuthCookie(tokenStr, 86400, Objects.equals(environment.getProperty("ENV"), ENV_PRODUCTION));
+            response.addHeader(HttpHeaders.SET_COOKIE, cookie);
+        }
+    }
+
 }
