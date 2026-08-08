f1f695552a2b6247df789bf802848add349c7631
imshahbaz21 <imshahbaz21@gmail.com>
2026-08-09 03:17:44 +0530

    added redis repo

---
+++ src/main/java/com/app/shahbaztrades/repo/redis/RedisCache.java
@@ -0,0 +1,36 @@
+package com.app.shahbaztrades.repo.redis;
+
+import org.redisson.api.RedissonClient;
+
+import java.time.Duration;
+
+public abstract class RedisCache {
+
+    protected final RedissonClient redissonClient;
+    private final String keyPrefix;
+
+    protected RedisCache(RedissonClient redissonClient, String keyPrefix) {
+        this.redissonClient = redissonClient;
+        this.keyPrefix = keyPrefix;
+    }
+
+    private String key(String id) {
+        return keyPrefix + ":" + id;
+    }
+
+    public <T> T get(String id) {
+        return redissonClient.<T>getBucket(key(id)).get();
+    }
+
+    public <T> void set(String id, T value, Duration ttl) {
+        redissonClient.<T>getBucket(key(id)).set(value, ttl);
+    }
+
+    public boolean exists(String id) {
+        return redissonClient.getBucket(key(id)).isExists();
+    }
+
+    public void delete(String id) {
+        redissonClient.getBucket(key(id)).delete();
+    }
+}
